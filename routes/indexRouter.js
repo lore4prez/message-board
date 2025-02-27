9a8d2e471075d3b80adb0b02088d944e37945116
Loren P <160626485+lore4prez@users.noreply.github.com>
2025-02-27 01:48:59 -0500

    Add index ejs template

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,8 +1,22 @@
 const { Router } = require('express');
 const router = Router();
 
+// Sample messages
+const messages = [
+    {
+        text: "Hi there!",
+        user: "Amando", 
+        added: new Date()
+    },
+    {
+        text: "I am Charles!",
+        user: "Charles", 
+        added: new Date()
+    },
+];
+
 router.get("/", (req, res, next) => {
-    res.send("You are in the index home page");
+    res.render("index", {title: "Mini Messageboard", msg: messages});
 });
 
 module.exports = router;
