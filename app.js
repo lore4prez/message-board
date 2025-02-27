3943dec75a2579a659a77270a1cdcedb06775964
Loren P <160626485+lore4prez@users.noreply.github.com>
2025-02-27 01:29:46 -0500

    Add two routes, / and /new

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const app = express();
 const port = process.env.PORT || 8000;
 const path = require('node:path');
 
+const indexRouter = require('./routes/indexRouter');
+const newMsgRouter = require('./routes/newMsgRouter');
+
 // Body parser middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -15,9 +18,8 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 // Routes
-app.get("/", (req, res, next) => {
-    res.send("Hi Im checking if server is working first");
-});
+app.use("/new", newMsgRouter);
+app.use("/", indexRouter);
 
 // Error Handler
 app.use((req, res, next) => {
