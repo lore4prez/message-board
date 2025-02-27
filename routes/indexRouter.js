58911edded289859b5e8586c07c698418cb79179
Loren P <160626485+lore4prez@users.noreply.github.com>
2025-02-27 18:22:38 -0500

    Fix html layout and Add post title

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -4,14 +4,16 @@ const router = Router();
 // Sample messages
 const messages = [
     {
+        title: "Introduction",
         text: "Hi there!",
         user: "Amando", 
-        added: new Date()
+        added: (new Date()).toDateString()
     },
     {
+        title: "Introduction",
         text: "I am Charles!",
         user: "Charles", 
-        added: new Date()
+        added: (new Date()).toDateString()
     },
 ];
 
@@ -26,7 +28,7 @@ router.post("/new", (req, res, next) => {
     const newPost = {
         user: req.body.author,
         text: req.body.message, 
-        added: new Date(),
+        added: (new Date()).toDateString(),
     };
 
     messages.push(newPost);
